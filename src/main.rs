84498c911df03a55829b8b92b441c0c835826c72
yumz24 <wvyy1234@gmail.com>
2026-05-08 23:15:21 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+// > gitx branch
+//
+// input:
+//  - type: feat
+//  - issue: 123
+//  - summary: login-api
+//
+// output
+// feat/#123-login-api
+
+fn main() {
+    println!("Hello, world!");
+}
